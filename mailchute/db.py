4586ad6c01e1b03cb76676db5e9ceecd5f0959d8
Kevin Qiu <kevin@freshbooks.com>
2014-08-31 21:02:29 -0400

    Fix supervisord environment problem

--- mailchute/db.py
+++ mailchute/db.py
@@ -1,10 +1,15 @@
 import sqlalchemy as sa
 from mailchute import settings
 from sqlalchemy.orm import scoped_session, sessionmaker
+from logbook import Logger
 
+
+logger = Logger(__name__)
+logger.info("DB_URL: {}".format(settings.DB['url']))
 
 engine = sa.create_engine(settings.DB['url'])
 
 Session = scoped_session(sessionmaker(bind=engine))
 
 session = Session()
+logger.debug("Session created")
--- mailchute/settings.py
+++ mailchute/settings.py
@@ -1,8 +1,18 @@
-from os import environ
+import os
+from logbook import Logger
+
+
+logger = Logger(__name__)
 
 
 def env(env_key, default):
-    return environ.get(env_key, default)
+    if env_key in os.environ:
+        result = os.environ[env_key]
+        logger.debug("Set {} from os.environ".format(env_key))
+    else:
+        result = default
+        logger.debug("Set {} to default".format(env_key))
+    return result
 
 
 DB = {
